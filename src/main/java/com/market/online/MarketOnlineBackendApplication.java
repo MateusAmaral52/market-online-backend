d92b309f82b2cd6b10a530e9dd188cad98fff8cb
Mateus Amaral <mateusamaral52@gmail.com>
2026-01-31 02:31:23 -0300

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/market/online/MarketOnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.market.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketOnlineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketOnlineBackendApplication.class, args);
+	}
+
+}
